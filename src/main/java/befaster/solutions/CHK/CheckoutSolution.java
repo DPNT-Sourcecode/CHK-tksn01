7458af46de52dd4f34a208c485e1fde09bc57100
sbx_user1051 <sbx_user1051@169.254.108.241>
2019-06-18 22:24:32 +0000

    Tue Jun 18 22:24:32 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,6 +1,8 @@
 package befaster.solutions.CHK;
 
 import befaster.solutions.CHK.domain.Item;
+import befaster.solutions.CHK.domain.Promotion;
+import befaster.solutions.CHK.exception.NoPromotionAvailableException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,20 +33,29 @@ public class CheckoutSolution {
         for (final Item item : skusWithOccurencesMap.keySet()) {
             final Integer numberOfOccurrences = skusWithOccurencesMap.get(item);
             int numberOfTimesToApplyPromotion;
-            switch (item) {
-                case A:
-                    numberOfTimesToApplyPromotion = numberOfOccurrences / 3;
-                    totalSum += numberOfOccurrences * item.getPrice() - numberOfTimesToApplyPromotion * 20;
-                    break;
-                case B:
-                    numberOfTimesToApplyPromotion = numberOfOccurrences / 2;
-                    totalSum += numberOfOccurrences * item.getPrice() - numberOfTimesToApplyPromotion * 15;
-                    break;
-                default:
-                    totalSum += numberOfOccurrences * item.getPrice();
+            Promotion promotion;
+            try {
+                promotion = Promotion.getByItem(item);
+                numberOfTimesToApplyPromotion = numberOfOccurrences / promotion.getQuantity();
+                totalSum += numberOfOccurrences * item.getPrice() - numberOfTimesToApplyPromotion * promotion.getSaving();
+            } catch (final NoPromotionAvailableException ignored) {
+                totalSum += numberOfOccurrences * item.getPrice();
             }
+//            switch (item) {
+//                case A:
+//                    numberOfTimesToApplyPromotion = numberOfOccurrences / 3;
+//                    totalSum += numberOfOccurrences * item.getPrice() - numberOfTimesToApplyPromotion * 20;
+//                    break;
+//                case B:
+//                    numberOfTimesToApplyPromotion = numberOfOccurrences / 2;
+//                    totalSum += numberOfOccurrences * item.getPrice() - numberOfTimesToApplyPromotion * 15;
+//                    break;
+//                default:
+//                    totalSum += numberOfOccurrences * item.getPrice();
+//            }
         }
 
         return totalSum;
     }
 }
+
