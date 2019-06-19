dc815a0446cdf8d40cbdcf1498b0b45cd165c5b2
sbx_user1051 <sbx_user1051@169.254.8.21>
2019-06-19 18:48:34 +0000

    Wed Jun 19 18:48:34 UTC 2019

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -3,13 +3,30 @@ package befaster.solutions.CHK;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 public class CheckoutSolutionTest {
 
+    private static final Map<String, Integer> TEST_CASES = new HashMap<String, Integer>() {{
+        put("AABCBD1A", -1);
+        put("", 0);
+        put(null, -1);
+        put("  ", -1);
+        put("ABCD", 115);
+        put("ABACA", 180);
+        put("DBBAC", 130);
+        put("ABABDAC", 210);
+        put("AAAAABAAAAA", 430);
+        put("AAAAA", 200);
+        put("AAAAAA", 250);
+        put("AABABAA", 245);
+        put("AAAAAAAA", 330);
+    }};
+
     private CheckoutSolution checkoutSolution;
-    private String skus;
-    private int result;
 
     @Before
     public void setUp() {
@@ -17,106 +34,8 @@ public class CheckoutSolutionTest {
     }
 
     @Test
-    public void should_return_default_value_for_invalid_input() {
-        givenInputIs("AABCBD1A");
-        whenICallCheckout();
-        thenResultIs(-1);
-    }
-
-    @Test
-    public void should_return_default_value_for_empty_string() {
-        givenInputIs("");
-        whenICallCheckout();
-        thenResultIs(0);
-    }
-
-    @Test
-    public void should_return_default_value_for_null_string() {
-        givenInputIs(null);
-        whenICallCheckout();
-        thenResultIs(-1);
-    }
-
-    @Test
-    public void should_return_default_value_for_blank_string() {
-        givenInputIs("  ");
-        whenICallCheckout();
-        thenResultIs(-1);
-    }
-
-    @Test
-    public void should_calculate_total_price_when_no_offers_are_present() {
-        givenInputIs("ABCD");
-        whenICallCheckout();
-        thenResultIs(115);
-    }
-
-    @Test
-    public void should_calculate_total_price_and_apply_promotion_1() {
-        givenInputIs("ABACA");
-        whenICallCheckout();
-        thenResultIs(180);
-    }
-
-    @Test
-    public void should_calculate_total_price_and_apply_promotion_2() {
-        givenInputIs("DBBAC");
-        whenICallCheckout();
-        thenResultIs(130);
-    }
-
-    @Test
-    public void should_calculate_total_price_and_apply_both_promotions() {
-        givenInputIs("ABABDAC");
-        whenICallCheckout();
-        thenResultIs(210);
-    }
-
-    @Test
-    public void should_apply_a_promotion_multiple_times_if_necessary() {
-        givenInputIs("AAAAABAAAAA");
-        whenICallCheckout();
-        thenResultIs(430);
-    }
-
-    @Test
-    public void should_apply_best_offer_for_an_item() {
-        givenInputIs("AAAAA");
-        whenICallCheckout();
-        thenResultIs(200);
-    }
-
-    @Test
-    public void should_apply_best_offer_for_an_item_and_combine_with_other_offers() {
-        givenInputIs("AAAAAA");
-        whenICallCheckout();
-        thenResultIs(250);
-    }
-
-    @Test
-    public void should_combine_best_offers_for_items() {
-        givenInputIs("AABABAA");
-        whenICallCheckout();
-        thenResultIs(245);
-    }
-
-    @Test
     public void should_combine_all_promotions_on_the_same_item_if_enough_qty() {
-        givenInputIs("AAAAAAAA");
-        whenICallCheckout();
-        thenResultIs(330);
-    }
-
-    private void givenInputIs(final String value) {
-        skus = value;
-    }
-
-    private void whenICallCheckout() {
-        result = checkoutSolution.checkout(skus);
-    }
-
-    private void thenResultIs(final int expectedValue) {
-        assertEquals(expectedValue, result);
+        TEST_CASES.forEach((skus, expectedTotal) -> assertEquals(expectedTotal, checkoutSolution.checkout(skus)));
     }
 }
 
